64e342b4877c27c08b7ba1039689615bf6038864
androu2048 <131658616+androu2048@users.noreply.github.com>
2023-04-24 13:44:06 +0400

    Initial create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let tg = window.Telegram.WebApp;
+let yob = document.getElementById("yobirth")
+let mob = document.getElementById("mobirth")
+let dob = document.getElementById("dobirth")
+let calc = document.getElementById("calculate")
+
+calc.addEventListener("click", () => {
+  
+});
