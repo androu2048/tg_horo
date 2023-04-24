1d5d7d8c1909396069ce61144d78915e920cc173
androu2048 <131658616+androu2048@users.noreply.github.com>
2023-04-24 14:49:26 +0400

    Add correct columns

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let tg = window.Telegram.WebApp;
-let yob = document.getElementById("yobirth")
-let mob = document.getElementById("mobirth")
-let dob = document.getElementById("dobirth")
-let start = document.getElementById("start")
-let calc = document.getElementById("calculate")
+let yob = document.getElementById("yobirth");
+let mob = document.getElementById("mobirth");
+let dob = document.getElementById("dobirth");
+let start = document.getElementById("start");
+let calc = document.getElementById("calculate");
 
 start.addEventListener("click", () => {
   document.getElementById("main").style.display = "none";
   document.getElementById("form").style.display = "block";
-  document.getElementById("yobirth").value = 1990
-  document.getElementById("mobirth").value = 01
-  document.getElementById("dobirth").value = 20
+  document.getElementById("yobirth").value = "1990";
+  document.getElementById("mobirth").value = "01";
+  document.getElementById("dobirth").value = "20";
 });
