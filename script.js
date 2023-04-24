70a3c00c373770062ea16a502c720a424ce561dc
androu2048 <131658616+androu2048@users.noreply.github.com>
2023-04-24 13:49:07 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,9 @@ let dob = document.getElementById("dobirth")
 let calc = document.getElementById("calculate")
 
 calc.addEventListener("click", () => {
-  
+  document.getElementById("main").style.display = "none";
+  document.getElementById("form").style.display = "block";
+  document.getElementById("yobirth").value = 1990
+  document.getElementById("mobirth").value = 01
+  document.getElementById("dobirth").value = 20
 });
