5ad3ec77c7ca06221f87e609b805a2605c558993
androu2048 <131658616+androu2048@users.noreply.github.com>
2023-04-24 14:44:06 +0400

    Correct button handler

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let tg = window.Telegram.WebApp;
 let yob = document.getElementById("yobirth")
 let mob = document.getElementById("mobirth")
 let dob = document.getElementById("dobirth")
+let start = document.getElementById("start")
 let calc = document.getElementById("calculate")
 
-calc.addEventListener("click", () => {
+start.addEventListener("click", () => {
   document.getElementById("main").style.display = "none";
   document.getElementById("form").style.display = "block";
   document.getElementById("yobirth").value = 1990
