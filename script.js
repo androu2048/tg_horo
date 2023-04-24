11a46e58f654d51016577f7d65f4f90268df7975
androu2048 <131658616+androu2048@users.noreply.github.com>
2023-04-24 19:45:11 +0400

    Rework event handler for single button

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 let tg = window.Telegram.WebApp;
 let start = document.getElementById("start");
-let calc = document.getElementById("calculate");
+let calculate = document.getElementById("calculate");
 tg.expand();
 
-start.addEventListener("click", () => {
-  document.getElementById("main").style.display = "none";
-  document.getElementById("form").style.display = "block";
-  document.getElementById("yobirth").value = "1990";
-  document.getElementById("mobirth").value = "01";
-  document.getElementById("dobirth").value = "20";
-});
-
-calc.addEventListener("click", () => {
+calculate.addEventListener("click", () => {
   let yob = document.querySelector("#year");
   let mob = document.querySelector("#month");
   let dob = document.querySelector("#day");
-  document.getElementById("main").style.display = "none";
+
+  document.getElementById("test").style.display = "none";
 
   let data = {
     yob: yob,
     mob: mob,
     dob: dob
-  }
-  tg.sendData(data)
-  
+  };
 
-  tg.close()
-})
+  tg.sendData(data);
+
+  tg.close();
+
+});
+
+start.addEventListener("click", () => {
+  document.getElementById("main").style.display = "none";
+  document.getElementById("form").style.display = "block";
+});
