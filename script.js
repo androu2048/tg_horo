d13584eaf8c287119afe2c47bcca17b58ca651f3
androu2048 <131658616+androu2048@users.noreply.github.com>
2023-04-24 15:12:11 +0400

    Pass data to tg

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let tg = window.Telegram.WebApp;
-let yob = document.getElementById("yobirth");
-let mob = document.getElementById("mobirth");
-let dob = document.getElementById("dobirth");
 let start = document.getElementById("start");
 let calc = document.getElementById("calculate");
+tg.expand();
 
 start.addEventListener("click", () => {
   document.getElementById("main").style.display = "none";
@@ -12,3 +10,20 @@ start.addEventListener("click", () => {
   document.getElementById("mobirth").value = "01";
   document.getElementById("dobirth").value = "20";
 });
+
+calc.addEventListener("click", () => {
+  let yob = document.querySelector("#year");
+  let mob = document.querySelector("#month");
+  let dob = document.querySelector("#day");
+  document.getElementById("main").style.display = "none";
+
+  let data = {
+    yob: yob,
+    mob: mob,
+    dob: dob
+  }
+  tg.sendData(data)
+  
+
+  tg.close()
+})
