3eec96697898815caaf88c931e4170a62d175cb3
androu2048 <131658616+androu2048@users.noreply.github.com>
2023-04-25 11:30:22 +0400

    Stringify data, minor markup changes

--- script.js
+++ script.js
@@ -4,20 +4,10 @@ let calculate = document.getElementById("calculate");
 tg.expand();
 
 calculate.addEventListener("click", () => {
-  let date = new Date(document.querySelector('input[type="date"]').value);
+  let birthday = new Date(document.querySelector('input[type="date"]').value);
 
-  let yob = date.getFullYear();
-  let mob = date.getMonth() + 1;
-  let dob = date.getDate();
-
-  let birthday = {
-    yob: yob,
-    mob: mob,
-    dob: dob
-  };
-
-  console.log(birthday);
-  tg.sendData(birthday);
+  console.log(JSON.stringify(birthday));
+  tg.sendData(JSON.stringify(birthday));
 
   tg.close();
 
