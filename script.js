8a7dd5cde9989600ff5245536e3c179963fc7a5b
androu2048 <131658616+androu2048@users.noreply.github.com>
2023-04-27 05:35:58 +0400

    Add day forecast menu

--- script.js
+++ script.js
@@ -4,17 +4,40 @@ tg.expand();
 
 let item = "";
 
-const wrapper = document.getElementById("wrapper");
+const sign_selector = document.getElementById("select-sign");
+const day_selector = document.getElementById("select-day");
+const day_btn = document.querySelector("#select-day .btn");
+const radio_buttons = document.querySelectorAll('input[name="day"]');
 
-wrapper.addEventListener("click", (event) => {
+
+sign_selector.addEventListener("click", (event) => {
 	
 	const isButton = event.target.nodeName === "BUTTON"
 	if (!isButton) {
-		tg.close();
+		return;
 	}
 	
-	tg.sendData(event.target.id);
+	sign_selector.style.display = "none";
+	day_selector.style.display = "inline-block";
+	
+	sign = event.target.id;
+	
+});
 
-	tg.close();
+day_btn.addEventListener("click", () => {
+	
+	let selected_day;
+	
+	for (const radio_button of radio_buttons) {
+		if (radio_button.checked) {			
+			selected_day = radio_button.value;
+			console.log(selected_day);
+			tg.sendData(sign, selected_day);
+
+			tg.close();
+			
+		}
+		else {}			
+	}
 
 });
